9909f5c30b39f5c470e48508b4f3f3d59eb6e302
muhammadus9139 <muhammadus9139@gmail.com>
2025-09-10 16:00:11 +0500

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,25 @@
+//NAME:Muhammad Usama
+//Reg no:sp25-bcs-151
+
+public class Student{
+   String name="usama";
+   String email="muhammadus9129@gmail.com";
+   double cgpa="4";
+
+  public void display(String name,String email,double cgpa){
+        System.out.println("Name="+s.name+",Email"+email+",cgpa="+cgpa);
+        
+    Student(String name,String email,double cgpa){
+        
+     }
+    public void setstudent(String name,String email,double cgpa){
+       this.name=name;
+        this.email=email;
+        this.cgpa=cgpa
+    }
+
+    public void getstudent(String name,String email,double cgpa){
+      this.name=name;
+       return this.name;
+      
+}
---
+++ demo.java
@@ -0,0 +1,10 @@
+public class Demo{
+    public static void main(String[] args){
+      
+      Student s=new Student();
+      s.display("name","email",cgpa);
+      s.setstudent("name","email",cgpa);
+      s.getstudent("name","email",cgpa);
+
+}
+
