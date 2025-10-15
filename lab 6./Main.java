6dc545cfe4670cb91dc0138f5d9ebbb7dd5da1e3
muhammadus9139 <muhammadus9139@gmail.com>
2025-10-15 18:54:56 +0500

    Add files via upload

---
+++ lab 6./Main.java
@@ -0,0 +1,6 @@
+public class Main{
+  public static void main(String args[]){
+     Plot p1=new Plot("003",Plot.Type.RES_5_MARLA,Plot.Shape.RECTANGLE,30,true);
+     System.out.println(p1.toString());
+     }
+}
